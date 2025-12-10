b62460d8084b313a5198281b4ac0cc9eb9bbdeea
Boluwatifeh <tifeasypeasy@gmail.com>
2025-12-11 00:03:17 +0100

    initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+// Ensure dotenv loads from project root
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+import { getDomainNameservers } from "../src/actions/getDomainNameservers.js";
+
+const config = {
+  endpoint: process.env.BASE_URL,  
+  username: process.env.EMAIL,
+  apiSecret: process.env.API_SECRET,
+};
+
+const params = { domain: "example.com" };
+
+const response = await getDomainNameservers(config, params, params.domain);
+console.log(response);
+
