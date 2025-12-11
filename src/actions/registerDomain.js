e7d4c405e3fb55759453bcfeb31125290746de43
Boluwatifeh <tifeasypeasy@gmail.com>
2025-12-11 01:17:07 +0100

    implement registerDomain functionality

--- src/actions/registerDomain.js
+++ src/actions/registerDomain.js
@@ -1,77 +1,22 @@
-import { registerDomain } from "../../index.js";
-import dotenv from "dotenv";
-dotenv.config();
+import { createAxiosClient } from "../config/axiosClient.js";
+import { generateToken } from "../utils/generateToken.js";
+import { formatError } from "../errors/handleError.js";
+import qs from "qs"; 
 
-const config = {
-  endpoint: process.env.BASE_URL,
-  username: process.env.EMAIL,
-  apiSecret: process.env.API_SECRET,
-};
-
-const whoisData = {
-  domain: "example.com",
-  regperiod: 1,
-  nameservers: {
-    ns1: "nsa.whogohost.com",
-    ns2: "nsb.whogohost.com",
-  },
-  contacts: {
-    registrant: {
-      firstname: "example",
-      lastname: "testing",
-      fullname: "example testing",
-      companyname: "textmachine",
-      email: "exam@gmail.com",
-      address1: "4 office",
-      city: "Lag",
-      state: "Lagos",
-      zipcode: "110001",
-      country: "NG",
-      phonenumber: "+234.812345678",
-    },
-    admin: {
-      firstname: "example",
-      lastname: "testing",
-      fullname: "example testing",
-      companyname: "textmachine",
-      email: "exam@gmail.com",
-      address1: "4 office",
-      city: "Lag",
-      state: "Lagos",
-      zipcode: "110001",
-      country: "NG",
-      phonenumber: "+234.812345678",
-    },
-    billing: {
-      firstname: "example",
-      lastname: "testing",
-      fullname: "example testing",
-      companyname: "textmachine",
-      email: "exam@gmail.com",
-      address1: "4 office",
-      city: "Lag",
-      state: "Lagos",
-      zipcode: "110001",
-      country: "NG",
-      phonenumber: "+234.812345678",
-    },
-    tech: {
-      firstname: "example",
-      lastname: "testing",
-      fullname: "example testing",
-      companyname: "textmachine",
-      email: "exam@gmail.com",
-      address1: "4 office",
-      city: "Lag",
-      state: "Lagos",
-      zipcode: "110001",
-      country: "NG",
-      phonenumber: "+234.87546898",
-    },
-  },
-};
-
-(async () => {
-  const result = await registerDomain(config, whoisData);
-  console.log(result);
-})();
+export async function registerDomain(config, whoisData) {
+  const { endpoint, username, apiSecret } = config;
+  const token = generateToken(username, apiSecret);
+  const client = createAxiosClient(endpoint);
+  try {
+    const response = await client.post(`/order/domains/register`, 
+      qs.stringify(whoisData), {
+        headers: {
+            username,
+            token, 
+        },
+    }); 
+    return response.data;
+  } catch (error) {
+    return formatError(error, "registerDomain");
+  }
+}
--- test/test.js
+++ test/test.js
@@ -9,7 +9,8 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // import { getEppCode } from "../src/actions/getEppCode.js";
-import { getRegistrarLockStatus } from "../src/actions/getRegistrarLock.js";
+// import { getRegistrarLockStatus } from "../src/actions/getRegistrarLock.js";
+import { registerDomain } from "../src/actions/registerDomain.js"; 
 
 const config = {
   endpoint: process.env.BASE_URL,  
@@ -17,8 +18,70 @@ const config = {
   apiSecret: process.env.API_SECRET,
 };
 
+const whoisData = {
+  domain: "example.com",
+  regperiod: 1,
+  nameservers: {
+    ns1: "nsa.whogohost.com",
+    ns2: "nsb.whogohost.com",
+  },
+  contacts: {
+    registrant: {
+      firstname: "example",
+      lastname: "testing",
+      fullname: "example testing",
+      companyname: "textmachine",
+      email: "exam@gmail.com",
+      address1: "4 office",
+      city: "Lag",
+      state: "Lagos",
+      zipcode: "110001",
+      country: "NG",
+      phonenumber: "+234.812345678",
+    },
+    admin: {
+      firstname: "example",
+      lastname: "testing",
+      fullname: "example testing",
+      companyname: "textmachine",
+      email: "exam@gmail.com",
+      address1: "4 office",
+      city: "Lag",
+      state: "Lagos",
+      zipcode: "110001",
+      country: "NG",
+      phonenumber: "+234.812345678",
+    },
+    billing: {
+      firstname: "example",
+      lastname: "testing",
+      fullname: "example testing",
+      companyname: "textmachine",
+      email: "exam@gmail.com",
+      address1: "4 office",
+      city: "Lag",
+      state: "Lagos",
+      zipcode: "110001",
+      country: "NG",
+      phonenumber: "+234.812345678",
+    },
+    tech: {
+      firstname: "example",
+      lastname: "testing",
+      fullname: "example testing",
+      companyname: "textmachine",
+      email: "exam@gmail.com",
+      address1: "4 office",
+      city: "Lag",
+      state: "Lagos",
+      zipcode: "110001",
+      country: "NG",
+      phonenumber: "+234.87546898",
+    },
+  },
+};
 const params = { domain: "example.com" };
 
-const response = await getRegistrarLockStatus(config, params.domain);
+const response = await registerDomain(config, whoisData);
 console.log(response);
 
